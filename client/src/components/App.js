2ecbe1da7ab34b404e16aabf9205d399cf3f4390
Gary <gomrdhl796@gmail.com>
2019-06-14 19:15:09 +0800

    change router to v4 setting

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import {Router , Switch , Route} from 'react-router-dom';
-import Home from '../containers/postList';
+import {Switch , Route} from 'react-router';
+import { ConnectedRouter } from 'connected-react-router'
+import Home from '../components/home';
 import Post from '../containers/addPost';
 import history from '../actions/history';
 import Article from '../containers/article';
 import Edit from '../containers/editPost';
 const App = () => (
-    <Router history={history}>
-        <Switch>
-        <Route exact path="/" component={Home}/>
-            <Route path="/post" component={Post}/>
-            <Route path="/article/:id/edit" component={Edit}/>
-            <Route path="/article/:id" component={Article}/>
-        </Switch>
-    </Router>
+  <ConnectedRouter history={history}>
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route path="/post" component={Post}/>
+      <Route path="/article/:id/edit" component={Edit}/>
+      <Route path="/article/:id" component={Article}/>
+    </Switch>
+  </ConnectedRouter>
 )
 
 export default App;
